f90d6a4ba2ed00aaf5f1d9625db4ad23a566ff5a
julianGonzalezV <j.andres2087@gmail.com>
2019-08-12 22:47:19 -0500

    operators and variables

---
+++ app01/src/_1_basic/Main.kt
@@ -0,0 +1,5 @@
+package _1_basic
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
---
+++ app01/src/_1_basic/Practice_1.kt
@@ -0,0 +1,134 @@
+package _1_basic
+
+import java.math.BigDecimal
+import java.math.BigInteger
+
+class OperatorsExample {
+    fun printHello () {
+        println ("Hello desde Kotlin")
+    }
+
+    /**
+     * Note como los primitivos tienen métodos y son tratados como instancia
+     */
+    fun primitveAsObjects(){
+        val fish = 20
+        println("Times => "+fish.times(6))
+        println("Div => "+ fish.div(10))
+        println("Plus => "+fish.plus(3))
+        println("Minus => "+fish.minus(3))
+        // Note la inmutabilidad de los vals
+        // fish = 21, no se puede reasignar
+        println("Vals son inmutables => "+fish)
+        println("llamado del metodo tostr => "+fish.toString())
+
+    }
+
+    fun boxing(){
+        val boxed:Number = 1
+        println("boxed => "+boxed)
+        boxed.toLong()
+    }
+
+    fun boxing2(){
+        val b:Byte = 1
+        val boxed:Number = b // todos los números proviene de Number
+        // val ln:BigInteger = // b no hay boxing
+        // se evita implicit numerical conversion por todos los errores que esto ha causado en la historia del java por ejemplo
+        // val i:Int = b //no hay boxing
+        //println("boxed => "+boxed)
+        println("toInt => "+b.toInt()) //mejor usar el casting asi
+
+    }
+
+    fun nullability(){
+        // val rock:Int = null , cuando se le dice el tipo de dato, en este caso Int no permite nulos, en su lugar ?
+        val rock:Int? = null
+        println("rock is  => "+rock)
+
+        /**
+         *OJO: importante notar que lotsOfFish NO es null, son sus elementos, por eso el signo ? dentro del diamond
+         * por eso sentencia como esta var lotsOfFish: List<String?> = null no va a funcionar , pues se le dijo
+         * que puede ser null en sus elementos
+         */
+        val lotsOfFish: List<String?> = listOf(null,null)
+        println("lotsOfFish is  => "+lotsOfFish)
+
+
+        val lotsOfFish2: List<String>? = null
+        println("lotsOfFish2 is  => "+lotsOfFish2) //  La lista pueder ser null
+        val lotsOfFish3: List<String?>? = null
+        println("lotsOfFish3 is  => "+lotsOfFish3) // ammos, la lista y sus elementos, pueden ser null
+
+    }
+
+    fun bangOperator(){
+        var v1:Int? = 2
+        /**
+         * Con !! (bang operator), validamos la posible nulidad de la variable
+         * lo que hace es generar un KotlinNullPointerException, note más abajo
+         */
+        println("bang v1 is  => "+v1!!.plus(2))
+
+        v1 = null
+        /**
+         * Note el KotlinNullPointerException
+         * LO QUE PARA EL DESARROLLADOR RESULTA MOLESTO, vaya a la siguiente funcion para que vea como se resuelve
+         */
+       // println("bang v1 is  => "+v1!!.plus(3))
+
+    }
+
+    fun easyNullValidation(){
+        var v1: Int? = 5
+        println("v1?.dec() #1 is  => "+v1?.dec()) // dec es la funcion para decrementar
+        v1 = null
+        println("v1?.dec() #2 is  => "+v1?.dec()) // dec es la funcion para decrementar
+    }
+
+    /**
+     * A partir de acá es realmente la practica 1 lo anterior son bases para poderla realizar
+     *
+     * 1)
+     * If you start with 2 fish, and they breed twice, producing 71 offspring the first time,
+     * and 233 offspring the second time, and then 13 fish are swallowed by a hungry moray eel,
+     * how many fish do you have left? How many aquariums do you need if you can put 30 fish per aquarium?
+     */
+    fun basicOperators(){
+        println(":::::::::::basicOperators:::::::::::::")
+        val fish = 2
+        val leftFishes = fish.plus(71).plus(233).minus(13)
+        println("leftFishes  => "+leftFishes)
+        println("aquariums  => "+ Math.ceil(leftFishes.toDouble().div(30)))
+    }
+
+    /**
+     * 1) Create a String variable rainbowColor, set its color value, then change it.
+     * 2) Create a variable blackColor whose value cannot be changed once assigned. Try changing it anyway.
+     */
+    fun variables(){
+        var rainbowColor = "yellow"
+        rainbowColor = "Blue"
+        val blackColor = "Black"
+        //blackColor = "b"//errror
+        // puedo crear otro val a partir de un val anterior, es decir que a pesar de ser uinmutable se puede
+        // enviar a otro computo
+        val blackAndWhite = blackColor.map{ x -> x+"1" }
+        println("blackAndWhite => "+blackAndWhite)
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val opVal: OperatorsExample = OperatorsExample()
+    opVal.printHello()
+    opVal.primitveAsObjects()
+    opVal.boxing()
+    opVal.boxing2()
+    opVal.nullability()
+    opVal.bangOperator()
+    opVal.easyNullValidation()
+    opVal.basicOperators()
+    opVal.variables()
+}
+
