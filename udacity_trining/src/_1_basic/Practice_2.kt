3c002d09fbe17cd83e1a2bbd1728748ce733f2e0
julianGonzalezV <j.andres2087@gmail.com>
2019-08-13 22:42:05 -0500

    study

---
+++ udacity_trining/src/_1_basic/Practice_2.kt
@@ -0,0 +1,45 @@
+package _1_basic
+
+class StringExamples{
+
+    fun concatExample(){
+        println("::::..concatExample:::::::")
+        val str1 = "I am"
+        val str2 = " a software engineer"
+        println("Suma => "+str1+str2)
+
+        /**
+         * Tambien se puede concatenar con numeros
+         */
+        val n1 = 5
+        val n2 = 12
+        val str3 = "I have $n1 uncles and $n2 cousins"
+        println(str3)
+        println("str3 => "+str3)
+        /**
+         * O tambien
+         */
+        val str4 = "I have ${n1+n2} uncles and cousins"
+        println("str4 => "+str4)
+
+    }
+
+    fun whenStatement(){
+        val fishName = "Juliancho carrancho"
+
+        when(fishName.length){
+            0 -> println("Error")
+            in 3..12 -> println("Good fish name")
+            else -> println("OK fish name")
+
+        }
+
+    }
+
+
+}
+
+fun main(args: Array<String>) {
+    StringExamples().concatExample()
+    StringExamples().whenStatement()
+}
---
+++ udacity_trining/src/_1_basic/Practice_3.kt
@@ -0,0 +1,130 @@
+package _1_basic
+
+import java.util.*
+import kotlin.Math.*
+
+class ArrysAndLoops{
+
+    /**
+     * Mutable list assigned to a inmutable variable
+     */
+    fun mmutableList(){
+        val list1 = mutableListOf("a","b","c")
+        //list1 = mutableListOf("d","e") // No se permite sobre val
+        /**
+         *
+         * Peeero si permite
+         */
+        list1.remove("c")
+        println("mmutableList:::::list1 => "+list1)
+    }
+
+    fun immutableList(){
+        val list1 = listOf("a","b","c")
+        //list1 = mutableListOf("d","e") // No se permite sobre val
+        /**
+         *
+         * Acá no se permite   el remove por ser un immutable list
+         */
+        //list1.remove("c")
+        print("InmmutableList:::::list1 => "+list1)
+    }
+
+
+    fun arrayElem(){
+        println("::::::arrayElem::::::::.")
+        //por inferencia
+        val l1 = arrayOf("a","b","c")
+
+        //Tipado
+        val l2 = intArrayOf(1,2,3)
+
+        //val l3 = Arrays.toString(intArrayOf(1,2,"3")) no va a funcionar porque como lo tipamos entonces
+        // no acepta el string de entrada
+        val l4 = Arrays.toString(arrayOf(1,2,"3")) // si funciona porque no está tipado
+        println(l4)
+
+        val arrayOfArray = arrayOf(l1,l2)
+        println("arrayOfArray => "+arrayOfArray)
+        val arrayOfArray2 = arrayOf(Arrays.toString(l1),Arrays.toString(l2))
+        println("Printable arrayOfArray2 => "+Arrays.toString(arrayOfArray2))
+    }
+
+    fun dynamicArrayElem() {
+        println("::::::dynamicArrayElem::::::::.")
+        val arr1 = Array(5){}
+        /**
+         * Note que cada registro en el array es de tipo unit
+         */
+        println("arr1=> "+Arrays.toString(arr1))
+
+        /**
+         * AHOra si el dinamismo :)
+         */
+        val arr2 = Array(5){it * 2} // a cada elemento lo multiplica por 2, es como un map en functional programming
+        println("arr2=> "+Arrays.toString(arr2))
+        /**
+         * Para evitar lo engorroso del array.tostr use asList
+         */
+        println("arr2 as list => "+arr2.asList())
+
+
+    }
+
+    fun loops(){
+        println("::::::loops::::::::.")
+        val arr1 = arrayListOf("a","e","i")
+        for (ele in arr1){
+            println(ele)
+        }
+
+        println("::::::loops tuple::::::::.")
+        /**
+         * Ojo importante decirle withIndex para que funcione
+         */
+        for ((i,e) in arr1.withIndex()){
+            println("$i - $e")
+        }
+
+        println("::::::Ranges::::::::.")
+        for (i in 'b'..'f') println(i)
+        for (i in 1..4) println(i)
+
+        println("::::::Ranges downwards::::::::.")
+        for (i in 'f' downTo 'b') println(i)
+
+        println("::::::Ranges steps::::::::.")
+        for(i in  3..9 step 2) println(i)
+    }
+
+    /**
+     * Read the code below, and then select the correct array initialization that will display
+     * the corresponding output
+     *  1 byte = 1 bytes
+        1 kilobyte = 1000 bytes
+        1 megabyte = 1000000 bytes
+        1 gigabyte = 1000000000 bytes
+        1 terabyte = 1000000000000 bytes
+        1 petabyte = 1000000000000000 bytes
+        1 exabyte = 1000000000000000000 bytes
+     */
+    fun questio1(){
+        //val array = ???
+        val v1 = 1000.0
+        val array = Array(7){ v1.pow(it) }
+        val sizes = arrayOf("byte", "kilobyte", "megabyte", "gigabyte",
+                "terabyte", "petabyte", "exabyte")
+        for ((i, value) in array.withIndex()) {
+            println("1 ${sizes[i]} = ${value.toLong()} bytes")
+        }
+    }
+
+}
+fun main(args: Array<String>){
+    ArrysAndLoops().mmutableList()
+    ArrysAndLoops().immutableList()
+    ArrysAndLoops().arrayElem()
+    ArrysAndLoops().dynamicArrayElem()
+    ArrysAndLoops().loops()
+
+}
