495d5876f9bac85de5c453e8ddf02bf8a05392d7
juligove <jagonzalezv@sura.com.co>
2019-08-30 13:53:48 -0500

    book

---
+++ udacity_trining/src/_0_book/_1_basic/Artist.kt
@@ -0,0 +1,10 @@
+package _0_book._1_basic
+
+/**
+ * Esto es more expressibe
+ */
+public class Artist(
+        var id: Long,
+        var name: String,
+        var url: String,
+        var mbid: String)
