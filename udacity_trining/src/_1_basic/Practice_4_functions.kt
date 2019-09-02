eaf92210f3017166130126e7fbe7ed859fa8ba44
juligove <jagonzalezv@sura.com.co>
2019-09-02 16:48:15 -0500

    udacity

--- udacity_trining/src/_1_basic/Practice_4_functions.kt
+++ udacity_trining/src/_1_basic/Practice_4_functions.kt
@@ -23,9 +23,98 @@ object Functions {
         println("Good ${if(arg[0].toInt() < 12) "Morning" else "Afternoon"}")
 
     }
+
+    fun feedTheFish(){
+        val day = randomDay()
+        val food = fishFood(day)
+        println("Today is $day and the fish eats $food ")
+    }
+
+    fun randomDay():String{
+        //val day = (0..)
+        val week = listOf("Lunes","Martes","Miercoles","Jueves","Viernes","Sábado","Domingo")
+        //return week.get((0..6).random())
+        //Acá 7 es excluido
+        return week.get(Random().nextInt(7))
+    }
+    fun fishFood(day: String):String{
+        val foodMap = mapOf("Lunes" to "flakes",
+        "Martes" to "frijoles",
+        "Miercoles" to "mondongo",
+        "Jueves" to "lentejas",
+        "Viernes" to "garbanzo",
+        "Sabado" to "sopa",
+        "Domingo" to "hamburguesa")
+        return foodMap[day]?:"Not found"
+    }
+
+    fun getFortuneCookie():String{
+        val mensaje = listOf("You will have a great day!","Things will go well for you today.","Enjoy a wonderful day of success",
+                "Be humble and all will turn out well.","Today is a good day for exercising restraint.","Take it easy and enjoy life!"
+                ,"Treasure your friends because they are your greatest fortune.")
+        print("Enter your birthday:")
+        val birthday = readLine()?.toIntOrNull()?:0
+        return mensaje[birthday.rem(mensaje.size)]
+    }
+
+    /**
+     * Versiòn mejorada porque no le corresponde calcular el birthay o recibirlo
+     * en su lugar usa a la funcion getBirthday()
+     */
+    fun getFortuneCookie2():String{
+        val mensaje = listOf("You will have a great day!","Things will go well for you today.","Enjoy a wonderful day of success",
+                "Be humble and all will turn out well.","Today is a good day for exercising restraint.","Take it easy and enjoy life!"
+                ,"Treasure your friends because they are your greatest fortune.")
+        print("Enter your birthday:")
+        when(getBirthday()){
+            in 28..31 -> return mensaje[2]
+            in 0..7 -> return mensaje[4]
+            else -> return mensaje[getBirthday().rem(mensaje.size)]
+        }
+    }
+
+    fun getBirthday():Int{
+        val birthday = readLine()?.toIntOrNull()?:0
+        return birthday
+    }
+
+    fun runNTime(n:Int){
+        for (i in  0..n){
+            val message = getFortuneCookie2()
+            if (message.contains("Take it easy")){
+                println("Congrats $message has been selected ")
+                break
+            }
+        }
+        println("End")
+    }
+
+    /**
+     * Note que si usted no pasa un valor entonces "fast serà el valor por defecto"
+     */
+    fun swim(speed:String = "fast"){
+        println("Swimming $speed")
+    }
+
 }
 
 fun main(args: Array<String>){
     Functions.dayOfWeek()
     Functions.passingArgsFromMain(args)
+    Functions.feedTheFish()
+    //print("day is => "+Functions.randomDay())
+    //deshabilite para que prueba :)
+    // Functions.runNTime(4)
+
+    /**
+     * Sin paràmetros
+     * Note como imprime fast
+     */
+    Functions.swim()
+
+    /**
+     * Con paràmetros
+     */
+    Functions.swim("leento")
+
 }
