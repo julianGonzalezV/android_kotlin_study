52fdf10c486d1c2663ba2e1c1aeab12081d95c74
julianGonzalezV <j.andres2087@gmail.com>
2020-01-20 22:40:53 -0500

    ejercicion

--- algorithmAndDataSt/src/co/com/study/examples/ListExamples.java
+++ algorithmAndDataSt/src/co/com/study/examples/ListExamples.java
@@ -15,11 +15,13 @@ public class ListExamples {
      */
     static List<List> duplicatedAndNodupItems(List<Integer> inputList){
         Set tempSet = new HashSet();
-        List duplicatedItems = new ArrayList();
-        List nonDuplicatedItems = new ArrayList();
+        Set nonDuplicatedItems = new HashSet();
+        Set duplicatedItems = new HashSet();
 
         for (Integer i:inputList) {
-            if(!tempSet.add(i)) duplicatedItems.add(i);
+            if(!tempSet.add(i)) {
+                duplicatedItems.add(i);
+            }
             else nonDuplicatedItems.add(i);
         }
         return new ArrayList(){{
@@ -35,11 +37,15 @@ public class ListExamples {
      */
     static List<List> duplicatedAndNodupItemsStream(List<Integer> inputList){
         Set temSet = new HashSet();
-        List nonDuplicated = new ArrayList();
-        List duplicated = new ArrayList();
+        Set nonDuplicated = new HashSet();
+        Set duplicated = new HashSet();
         inputList.stream().map(x -> {
-            if(temSet.add(x)) nonDuplicated.add(x);
-            else duplicated.add(x);
+            if(temSet.add(x))nonDuplicated.add(x);
+
+            else {
+                duplicated.add(x);
+                nonDuplicated.remove(x);
+            }
             return x;
         }).collect(Collectors.toList());
 
@@ -52,17 +58,39 @@ public class ListExamples {
     }
     static int mostRepeated(List<Integer> input){
         Set  set = new HashSet();
-        Map map = new HashMap();
+        Map<Integer,Integer> map = new HashMap();
         for (Integer i:input) {
-            map.put(i,1);
+            Integer val = map.remove(i);
+            map.put(i, (val != null)? val+1: 1);
         }
+        map.keySet();
+        System.out.println("map"+map);
+        /*Map<Integer, Integer> sortedIdNameMap
+                = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+*/
+        Map<Integer,ArrayList> maR = new HashMap<>();
+        map.forEach((k,v)->{
+                  if(maR.containsKey(v)) {
+                      ArrayList value = maR.get(v);
+                      value.add(k);
+                      maR.put(v,value);
+                  }else{
+                      maR.put(v,new ArrayList(){{add(k);}});
+                  }
+        });
 
+        System.out.println("values"+maR);
         return 0;
     }
 
     public static void main (String ... args){
         System.out.println(duplicatedAndNodupItems(Arrays.asList(1,2,3,3,4,5,6,5,7,8)));
         System.out.println(duplicatedAndNodupItemsStream(Arrays.asList(1,2,3,3,4,5,6,5,7,8)));
+        System.out.println(mostRepeated(Arrays.asList(1,2,3,3,4,5,6,5,7,8)));
+
 
     }
 }
